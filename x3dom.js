3d182ad7dd7f9b0f564d2e0a3cdc2196efa693d9
p4bloch <pchiappetti@gmail.com>
2014-10-14 16:58:06 -0300

    Initial commit

---
+++ x3dom.js
@@ -0,0 +1,43 @@
+Boxes = new Meteor.Collection("boxes");
+
+if (Meteor.isClient) {
+  Session.set("color", "#aaa");
+
+UI.body.helpers({
+  boxes: function () {
+    return Boxes.find();
+  },
+  colors: ["#FFBF27", "#E17244", "#483F36", "#4BC5FF", "#F7AE52"]
+});
+
+UI.body.events({
+  "click .swatch": function () {
+    Session.set("color", this.valueOf());
+  },
+  "mousedown x3d": function () {
+    dragged = false;
+  },
+  "mousemove x3d": function () {
+    dragged = true;
+  },
+  "mouseup shape": function (evt) {
+    if (!dragged && evt.button === 1) {
+      Boxes.insert({
+        color: Session.get("color"),
+        x: Math.floor(evt.worldX + evt.normalX / 2) + 0.5,
+        y: Math.floor(evt.worldY + evt.normalY / 2) + 0.5,
+        z: Math.floor(evt.worldZ + evt.normalZ / 2) + 0.5
+      });
+    } else if (!dragged &&
+      (evt.button === 4 || evt.button === 2)) {
+        Boxes.remove(evt.currentTarget.id);
+    }
+  }
+});
+}
+
+// if (Meteor.isServer) {
+//   Meteor.startup(function () {
+//     // code to run on server at startup
+//   });
+// }
